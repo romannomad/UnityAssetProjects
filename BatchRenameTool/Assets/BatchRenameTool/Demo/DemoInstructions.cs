326521d4c712282fe5f19dd0919483be81dc8900
Roman <63406202+romannomad@users.noreply.github.com>
2026-04-14 14:54:55 +0300

    Update

--- BatchRenameTool/Assets/BatchRenameTool/Demo/DemoInstructions.cs
+++ BatchRenameTool/Assets/BatchRenameTool/Demo/DemoInstructions.cs
@@ -1,12 +1,17 @@
-using UnityEngine;
-
-public class DemoInstructions : MonoBehaviour
+namespace BatchRenameTool
 {
-    [TextArea]
 
-    public string instructions =
-    "This demo scene is included.\n\n" +
-        "To open the Batch Rename Tool:\n" +
-        "Tools → Batch Rename Tool\n\n" +
-        "This scene does not require runtime functionality.";
+
+    using UnityEngine;
+
+    public class DemoInstructions : MonoBehaviour
+    {
+        [TextArea]
+
+        public string instructions =
+        "This demo scene is included.\n\n" +
+            "To open the Batch Rename Tool:\n" +
+            "Tools → Batch Rename Tool\n\n" +
+            "This scene does not require runtime functionality.";
+    }
 }
