dc1f5a11b184249dcb591ebe0284cdb3807b1d64
Roman <63406202+romannomad@users.noreply.github.com>
2026-04-14 14:00:36 +0300

    update

--- BatchRenameTool/Assets/BatchRenameTool/Demo/DemoInstructions.cs
+++ BatchRenameTool/Assets/BatchRenameTool/Demo/DemoInstructions.cs
@@ -5,7 +5,7 @@ public class DemoInstructions : MonoBehaviour
     [TextArea]
 
     public string instructions =
-    "This demo scene is included to satisfy Unity Asset Store validation.\n\n" +
+    "This demo scene is included.\n\n" +
         "To open the Batch Rename Tool:\n" +
         "Tools → Batch Rename Tool\n\n" +
         "This scene does not require runtime functionality.";
