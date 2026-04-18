6ee4dbf723c3318d37f1ddfabb860f4348067065
Roman <63406202+romannomad@users.noreply.github.com>
2026-04-18 11:35:49 +0200

    Update

--- BatchRenameTool/Assets/BatchRenameTool/Demo/DemoInstructions.cs
+++ BatchRenameTool/Assets/BatchRenameTool/Demo/DemoInstructions.cs
@@ -11,7 +11,7 @@ namespace BatchRenameTool
 
         //Instructions for Batch Rename Tool demo scene
         public string instructions =
-        "This demo shows how to use the Batch Rename Tool.\n\n" +
+        "This demo shows how to use the Batch Rename Tool..\n\n" +
             "If you want to open the Batch Rename Tool:\n" +
             "Tools → Batch Rename Tool\n\n" +
             "This scene does not require runtime functionality.";
